25571b6c9b7159215adbb92888dcf25ee0aac03a
veeranaidu2 <71113273+veeranaidu2@users.noreply.github.com>
2020-12-22 12:46:31 -0600

    write issues to csv

--- collect_github_issues.py
+++ collect_github_issues.py
@@ -4,10 +4,18 @@ def search_github(org_name, repo_name):
     g = Github("ACCESS-TOKEN-GOES-HERE")
     res = g.get_organization(org_name)
 
+    #Will be refactored to check each page.
     for repo in res.get_repos():
-        if repo.name == repo_name:
-            print(f'{repo.open_issues_count}')
+        if repo.name == "elasticsearch":
+            issues_object = repo.get_issues()
             break
+
+    with open('issues.csv', mode='w') as csv_file:
+        issue_writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        for issue in issues_object:
+            issue_writer.writerow([issue.id, issue.title, issue.state, issue.created_at, issue.html_url])
+
+
             
 
 if __name__ == '__main__':
