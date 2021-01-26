ad89981eeab72733d69a754e2271e6d494a87bcf
bramha574 <veeranaidu2@gmail.com>
2021-01-25 21:06:44 -0600

    add code to mine github repositories

--- collect_issues.py
+++ collect_issues.py
@@ -3,18 +3,6 @@ import csv
 import sys
 import getopt
 import os
-import requests
-
-def get_buggy_files(repo, pull_request):
-    requests.get("https://api.github.com/user", auth=("78cf2efd43bb785275007bf44c169a00e2d35727", "x-oauth-basic"))
-    files_list = []
-    pull_request_id = int(pull_request.html_url.split("/")[-1])
-    github_files = (repo.get_pull(pull_request_id)).get_files()
-
-    for github_file in github_files:
-        files_list.append(github_file.filename)
-
-    return files_list
 
 
 def search_github(full_repo_name):
@@ -22,16 +10,11 @@ def search_github(full_repo_name):
     repo_name = full_repo_name.split('/')[1]
     repo_object = None
 
-    issue_summary_file = f'{org_name}-{repo_name}-issues'
+    issue_summary_file = f'{org_name}-{repo_name}'
 
     issues_object = None
-    client = Github('78cf2efd43bb785275007bf44c169a00e2d35727')
-
-    # ACCESS_USERNAME = 'veeranaidu2'
-    # ACCESS_PWD = "Bramha@574"
-    # client = Github(ACCESS_USERNAME, ACCESS_PWD)
-
-    res = client.get_organization(org_name)
+    g = Github("bd71ca67d51f9fa6f0280c008b480b76e3bb1e81")
+    res = g.get_organization(org_name)
 
     for repo in res.get_repos():
         if repo.name == repo_name:
@@ -44,15 +27,14 @@ def search_github(full_repo_name):
 
     with open(f'{issue_summary_file}.csv', mode='a', newline='', encoding='utf-8') as csv_file:
         issue_writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        issue_writer.writerow(["ISSUE ID", "ISSUE_TITLE", "FILES", "ISSUE STATE", "ISSUE CREATED AT", "ISSUE URL"])
+        issue_writer.writerow(["ISSUE_ID", "ISSUE_TITLE", "ISSUE_LABELS", "ISSUE_STATE", "ISSUE_CREATED_AT", "ISSUE_URL"])
         for issue in issues_object:
             for label in issue.labels:
                 if label.name.lower() == "bug":
                     if issue.labels:
-                        if issue.pull_request:
-                            buggy_files = get_buggy_files(repo_object, issue.pull_request)
-                            for file_name in buggy_files:
-                                issue_writer.writerow([issue.id, issue.title, file_name, issue.state, issue.created_at, issue.html_url])
+                        print(f"Writing issue {issue.id}")
+                        issue_writer.writerow([issue.id, issue.title, label.name.lower(), issue.state, issue.created_at, issue.html_url])
+
 
 def evaluate_args():
     argument_list = sys.argv[1:]
